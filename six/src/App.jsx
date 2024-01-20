f2f29cf879b470bc2097feacbaa4e1c1424c2aee
Artuur Alexandrov <naturahxq@gmail.com>
2024-01-20 18:35:28 +0300

    Простая модалка

--- six/src/App.jsx
+++ six/src/App.jsx
@@ -2,28 +2,38 @@ import { useState } from "react";
 import "./App.scss";
 
 export default function App() {
-  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(false);
 
-  function valueDecrement() {
-    setValue(value - 1);
+  function visibleOpen() {
+    setVisible(true);
   }
 
-  function valueIncrement() {
-    setValue(value + 1);
+  function visibleClose() {
+    setVisible(false);
   }
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{value}</h1>
-        <button className="minus" onClick={valueDecrement}>
-          - Минус
+      {visible ? (
+        <div className="overlay">
+          <div className="modal">
+            <svg
+              onClick={visibleClose}
+              height="200"
+              viewBox="0 0 200 200"
+              width="200"
+            >
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
+        </div>
+      ) : (
+        <button onClick={visibleOpen} className="open-modal-btn">
+          ✨ Открыть окно
         </button>
-        <button className="plus" onClick={valueIncrement}>
-          Плюс +
-        </button>
-      </div>
+      )}
     </div>
   );
 }
