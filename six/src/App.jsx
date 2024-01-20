73fd494bfb9d6fa966563cdc84417d85680ab7db
Artuur Alexandrov <naturahxq@gmail.com>
2024-01-20 20:43:23 +0300

    Quiz in React

--- six/src/App.jsx
+++ six/src/App.jsx
@@ -1,38 +1,84 @@
 import { useState } from "react";
 import "./App.scss";
 
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result() {
+  const qulen = questions.length;
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 2 ответа из 3</h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((obj, index) => (
+          <li onClick={() => onClickVariant(index)} key={index}>
+            {obj}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 export default function App() {
-  const [visible, setVisible] = useState(false);
+  const [step, setStep] = useState(0);
+  const [trues, setTrues] = useState(0);
 
-  function visibleOpen() {
-    setVisible(true);
-  }
+  const question = questions[step];
 
-  function visibleClose() {
-    setVisible(false);
+  function stepClick() {
+    setStep(step + 1);
   }
 
   return (
     <div className="App">
-      {visible ? (
-        <div className="overlay">
-          <div className="modal">
-            <svg
-              onClick={visibleClose}
-              height="200"
-              viewBox="0 0 200 200"
-              width="200"
-            >
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          </div>
-        </div>
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={stepClick} />
       ) : (
-        <button onClick={visibleOpen} className="open-modal-btn">
-          ✨ Открыть окно
-        </button>
+        <Result />
       )}
     </div>
   );
