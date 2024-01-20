d6ea3c7e741c1c7c937d744c11a80d9d14815f1b
Artuur Alexandrov <naturahxq@gmail.com>
2024-01-20 18:13:09 +0300

    counter

--- six/src/App.jsx
+++ six/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 
 export default function App() {
+  const [value, setValue] = useState(0);
+
+  function valueDecrement() {
+    setValue(value - 1);
+  }
+
+  function valueIncrement() {
+    setValue(value + 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{value}</h1>
+        <button className="minus" onClick={valueDecrement}>
+          - Минус
+        </button>
+        <button className="plus" onClick={valueIncrement}>
+          Плюс +
+        </button>
       </div>
     </div>
   );
